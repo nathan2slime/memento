7145d8c48376c0ebbd9ae5fb5f9dc9e67b1d71e5
Jhonathan <francisco.cajlon-ext@casasbahia.com.br>
2025-08-27 20:31:57 -0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('expo/metro-config').MetroConfig}
+ */
+const { getDefaultConfig } = require('expo/metro-config')
+const { withTamagui } = require('@tamagui/metro-plugin')
+
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+})
+
+config.resolver.sourceExts.push('mjs')
+
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+})
